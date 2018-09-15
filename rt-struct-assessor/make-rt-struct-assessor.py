7ab67112fd6918d01d8dfa1f1b622d91359f912c
John Flavin <flavinj@gmail.com>
2018-09-14 20:31:37 -0500

    rt-struct roi collection assessor now has series of referenced study UIDs

--- rt-struct-assessor/make-rt-struct-assessor.py
+++ rt-struct-assessor/make-rt-struct-assessor.py
@@ -59,7 +59,8 @@ tags = {
     'SOPInstanceUID': (0x0008, 0x0018),
     'StudyDate': (0x0008, 0x0020),
     'StudyTime': (0x0008, 0x0030),
-    'StructureSetLabel': (0x3006, 0x0002)
+    'StructureSetLabel': (0x3006, 0x0002),
+    'ReferencedFrameOfReferenceSequence': (0x3006, 0x0010)
 }
 
 with open(dicom_path, 'rb') as f:
@@ -73,7 +74,17 @@ name = dicom[tags['StructureSetLabel']].value
 date =  dicom[tags['StudyDate']].value if tags['StudyDate'] in dicom else "yyyymmdd"
 time_header = dicom[tags['StudyTime']].value if tags['StudyTime'] in dicom else "hhmmss"
 time = time_header if '.' not in time_header else time_header[:time_header.index('.')]
-assessor_label = "{}_RTSTRUCT_{}_{}".format(session_label, date, time)
+
+# Need to get through a couple nested sequences to get to (0020, 000E)
+# https://dicom.innolitics.com/ciods/rt-structure-set/structure-set/30060010/30060012/30060014/0020000e
+# We need to go (3006, 0010) -> (3006, 0012) -> (3006, 0014) -> (0020, 000E)
+referencedStudyUids = []
+for referencedFrameOfReference in dicom[tags['ReferencedFrameOfReferenceSequence']]:
+    for rtReferencedStudy in referencedFrameOfReference[(0x3006, 0x0012)]:
+        for rtReferencedSeries in rtReferencedStudy[(0x3006, 0x0014)]:
+            referencedStudyUids.append(rtReferencedSeries[(0x0020, 0x000E)].value)
+
+assessor_label = "{}_RTSTRUCT_{}{}".format(session_label, date, "_" + time if time != "hhmmss" else "")
 assessor_id = '{}_RTSTRUCT_{}'.format(session_id, uuid.uuid4())
 
 assessorElements = [
@@ -97,7 +108,11 @@ E = ElementMaker(namespace=nsdict['icr'], nsmap=nsdict)
 assessorXML = E('RoiCollection', assessorTitleAttributesDict,
     E(ns('xnat', 'date'), dt.date.today().isoformat()),
     E(ns('xnat', 'imageSession_ID'), session_id),
-    *[E(name, value) for name, value in assessorElements]
+    E('UID', uid),
+    E('collectionType', 'RTSTRUCT'),
+    E('subjectID', subject_id),
+    E('references', *[E('seriesUID', seriesUid) for seriesUid in referencedStudyUids]),
+    E('name', name)
 )
 
 print('Writing assessor XML to {}'.format(assessor_xml_path))
