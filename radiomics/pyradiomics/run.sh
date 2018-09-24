39146d69ab6695d6d6087e5e0d03797a6335d7e8
John Flavin <flavinj@gmail.com>
2018-09-24 15:49:15 -0500

    Create pyradiomics image that takes NRRD inputs

---
+++ radiomics/pyradiomics/run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+
+die() {
+    echo "@" 2>&1
+    exit 1
+}
+
+scan="$1"
+shift
+mask="$1"
+shift
+output="$1"
+shift
+project="$1"
+shift
+session_id="$1"
+shift
+session_label="$1"
+shift
+scan_id="$1"
+shift
+mask_file_uri="$1"
+
+echo "Scan file: ${scan}"
+echo "Mask file: ${mask}"
+echo "Output dir: ${output}"
+echo "Project: ${project}"
+echo "Session ID: ${session_id}"
+echo "Session label: ${session_label}"
+echo "Scan ID: ${scan_id}"
+echo "Mask File URI: ${mask_file_uri}"
+
+# # Begin constructing CSV batch file
+# batch_file="/tmp/batch.csv"
+# echo "Image,Mask,Label" > ${batch_file}
+
+
+# mask_file_name=$(basename $mask)
+# echo "Mask file name: $mask_file_name"
+#
+# echo "Running find-mask-value ${mask}"
+# mask_label_value=$(find-mask-value ${mask})
+# echo "Mask label value: $mask_label_value"
+
+# echo "${scan},${mask},${mask_label_value}" >> ${batch_file}
+
+echo
+echo "Running pyradiomics"
+echo "pyradiomics ${scan} ${mask} --param /usr/src/pyradiomics/examples/exampleSettings/exampleMR_3mm.yaml --verbosity 5 --out ${output}/pyradiomics.csv --format-path basename --format csv"
+pyradiomics ${scan} ${mask} --param /usr/src/pyradiomics/examples/exampleSettings/exampleMR_3mm.yaml --verbosity 5 --out ${output}/pyradiomics.csv --format-path basename --format csv || die "pyradiomics failed"
+
+echo
+
+echo "Generating assessor"
+echo "generate-pyradiomics-xml-from-csv $XNAT_HOST $project $session_id $session_label $scan_id $mask_file_uri ${output}/pyradiomics.csv ${output}/pyradiomics.xml"
+generate-pyradiomics-xml-from-csv $XNAT_HOST $project $session_id $session_label $scan_id $mask_file_uri ${output}/pyradiomics.csv ${output}/pyradiomics.xml || die "failed to generate assessor"
+
+echo
+echo "All done"
