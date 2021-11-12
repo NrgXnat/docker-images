7405944dfb7c0e18f0dc2b007a5c4c5df0a3ce39
Kate Alpert <kate@radiologics.com>
2021-11-12 15:40:00 -0700

    spreadsheet-importer: add exclude param and flush prints

--- spreadsheet-importer/import.py
+++ spreadsheet-importer/import.py
@@ -11,6 +11,7 @@ parser.add_argument("--spreadsheet", help="Spreadsheet to import (csv or Excel)"
 parser.add_argument("--labelColumn", help="Column that identifies the object in XNAT", required=False, default="")
 parser.add_argument("--create", action="store_true", help="Create XNAT object if it does not exist", required=False)
 parser.add_argument("--lut", help="Look up table matching header value (column 1) to XNAT variable (column 2). First line ignored", required=False)
+parser.add_argument("--exclude", help="Columns to exclude", required=False)
 #TODO add support for other datatypes
 #parser.add_argument("--datatype", help="Type of XNAT object (Subject or Experiment)", required=False, default="Experiment")
 
@@ -25,28 +26,34 @@ def getOrCreate(label):
     if label in objects:
         xobject = objects[label]
     elif args.create:
-        print("Creating subject {}".format(label))
+        print("Creating subject {}".format(label), flush=True)
         xobject = connection.classes.SubjectData(parent=project, label=label)
     else:
         raise Exception("ERROR: create=False and no existing subject with label {}".format(label))
     return xobject
 
 
-def populateVariables(xobject, row, lut):
-    print("Populating variables for subject {}".format(xobject.label))
+def populateVariables(xobject, row, lut, exclude):
+    print("Populating variables for subject {}".format(xobject.label), flush=True)
     params = {}
     for key in row.keys():
-        if not pd.isna(row[key]):
-            if key in lut:
-                xnat_var = lut[key]
-                if xnat_var == "group":
-                    var = "xnat:subjectData/group"
-                else:
-                    var = "xnat:subjectData/demographics[@xsi:type=xnat:demographicData]/{}".format(xnat_var)
+        if key in exclude:
+            continue
+        if pd.isna(row[key]):
+            continue
+        value = row[key]
+        if key in lut:
+            xnat_var = lut[key]
+            if xnat_var == "group":
+                var = "xnat:subjectData/group"
             else:
-                #xobject.fields[key.lower()] = row[key]
-                var = "xnat:subjectData/fields/field[name={}]/field".format(removeSpecialChars(key.lower()))
-            params[var] = row[key]
+                var = "xnat:subjectData/demographics[@xsi:type=xnat:demographicData]/{}".format(xnat_var)
+                if xnat_var == "yob" or xnat_var == "age":
+                    value = int(value)
+        else:
+            #xobject.fields[key.lower()] = row[key]
+            var = "xnat:subjectData/fields/field[name={}]/field".format(removeSpecialChars(key.lower()))
+        params[var] = value
     r = connection.put(xobject.uri, data = params)
     r.raise_for_status()
 
@@ -72,8 +79,13 @@ lut = {}
 if args.lut:
     t = pd.read_csv(args.lut)
     for r in t.itertuples(index=False):
-        print("Mapping {} to XNAT variable {}".format(r[0], r[1]))
+        print("Mapping {} to XNAT variable {}".format(r[0], r[1]), flush=True)
         lut[r[0]] = r[1]
+
+exclude = []
+if args.exclude:
+    exclude = [s.strip() for s in args.exclude.split(",")]
+    print("Excluding: {}".format(exclude), flush=True)
 
 errors = False
 try:
@@ -82,8 +94,9 @@ try:
     for index, row in df.iterrows():
         try:
             xobject = getOrCreate(removeSpecialChars(str(row[args.labelColumn])))
-            populateVariables(xobject, row, lut)
+            populateVariables(xobject, row, lut, exclude)
         except Exception as e:
+            print("Error for {}".format(row), flush=True)
             printErr(str(e))
             errors = True
 finally:
